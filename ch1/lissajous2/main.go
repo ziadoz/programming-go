bfb179c512fa77e10cfe27a470bd9c117c1942f2
Jamie <ziadoz@gmail.com>
2017-05-16 20:12:49 +0100

    Complete lissajous 1.5 exercise

---
+++ ch1/lissajous2/main.go
@@ -0,0 +1,58 @@
+// Lissajous generates GIF animations of random Lissajous figures.
+// 1.5 - Change the background colour to black and the wave colour to green.
+package main
+
+import (
+    "image"
+    "image/color"
+    "image/gif"
+    "io"
+    "math"
+    "math/rand"
+    "os"
+    "time"
+)
+
+var palette = []color.Color{color.Black, color.RGBA{0, 128, 0, 1}}
+
+const (
+    bgIndex = 0 // First colour in palette.
+    fgIndex = 1 // Next colour in palette.
+)
+
+func main() {
+    rand.Seed(time.Now().UTC().UnixNano())
+    lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+    const (
+        cycles  = 5     // Number of complete x oscillator revolutions.
+        res     = 0.001 // Angular resolution.
+        size    = 100   // Image canvas covers [-size..+size]
+        nFrames = 64    // Number of animation frames.
+        delay   = 8     // Delay between frames in 10ms units.
+    )
+
+    freq  := rand.Float64() * 3.0 // Relative frequency of y oscillator.
+    anim  := gif.GIF{LoopCount: nFrames}
+    phase := 0.0 // Phase difference.
+
+    for i := 0; i < nFrames; i++ {
+        rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+        img  := image.NewPaletted(rect, palette)
+
+        for t := 0.0; t < cycles*2*math.Pi; t += res {
+            x := math.Sin(t)
+            y := math.Sin(t*freq + phase)
+
+            img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), fgIndex)
+        }
+
+        phase += 0.1
+        anim.Delay = append(anim.Delay, delay)
+        anim.Image = append(anim.Image, img)
+    }
+
+    gif.EncodeAll(out, &anim) // Note: Ignoring encoding errors.
+}
