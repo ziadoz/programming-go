b37a79dd49eb5729efe669af99eac59509045a7d
Jamie <ziadoz@gmail.com>
2017-05-20 15:44:56 +0100

    Server1

---
+++ ch1/server1/main.go
@@ -0,0 +1,17 @@
+// Server1 is a minimal "echo" server.
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", handler) // Each request calls the handler function.
+    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
