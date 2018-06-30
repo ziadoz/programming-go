0fa66aad3212e76611b56f8ad3ef3b8beecf6c5c
Jamie <ziadoz@gmail.com>
2018-06-30 12:52:36 +0100

    Sort slice by CreatedAt. Display CreatedAt.

--- ch4/issues/main.go
+++ ch4/issues/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 
 	"gopl.io/ch4/github"
 )
@@ -15,14 +16,22 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Sort slices by time descending.
+	sort.Slice(result.Items, func(i, j int) bool {
+		a := result.Items[i]
+		b := result.Items[j]
+		return a.CreatedAt.After(b.CreatedAt)
+	})
+
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
 		// Field formatting:
-		// Literal # Symbol
-		// 5 Digits, Padded Right
-		// 9 String Character Width, 9 Precision
-		// Default Character Width, 55 Precision
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+		// - Literal # Symbol
+		// - 5 Digits, Padded Right
+		// - String
+		// - 9 String Character Width, 9 Precision
+		// - Default Character Width, 55 Precision
+		fmt.Printf("#%-5d %s %9.9s %.55s\n",
+			item.Number, item.CreatedAt.Format("2006-01-02"), item.User.Login, item.Title)
 	}
 }
