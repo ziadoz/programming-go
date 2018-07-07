a8a06aa257858e67bc085038d44c20f80e954ed6
Jamie <ziadoz@gmail.com>
2018-07-07 12:36:43 +0100

    Autoescaping in HTML templates (and how to ignore it for trusted data).

---
+++ ch4/autoescape/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+func main() {
+	const templ = `<p>A: {{.A}}</p><p>B: {{.B}}</p>`
+	t := template.Must(template.New("escape").Parse(templ))
+
+	var data struct {
+		A string        // Untrusted plain text.
+		B template.HTML // Trusted HTML.
+	}
+
+	data.A = "<b>Hello!</b>"
+	data.B = "<b>Hello!</b>"
+
+	if err := t.Execute(os.Stdout, data); err != nil {
+		log.Fatal(err)
+	}
+}
