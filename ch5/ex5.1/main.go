00ca18ab7917afce9614c56fcf2d217d05e9e0c2
Jamie <ziadoz@gmail.com>
2018-07-16 21:42:04 +0100

    Exercise 5.1. Recursive link visiting.

---
+++ ch5/ex5.1/main.go
@@ -0,0 +1,41 @@
+// Usage: curl https://www.bbc.co.uk | go run main.go
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, node *html.Node) []string {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		for _, a := range node.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	if node.FirstChild != nil {
+		links = visit(links, node.FirstChild)
+	}
+
+	if node.NextSibling != nil {
+		links = visit(links, node.NextSibling)
+	}
+
+	return links
+}
