10cc60c24765a0bc704a1e4f9a1c846272790586
Jamie <ziadoz@gmail.com>
2018-08-20 20:04:21 +0100

    Title 1/2. Using defer to handle resource cleanup.

---
+++ ch5/title2/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		err := title(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func title(url string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code %d", response.StatusCode)
+	}
+
+	// Check content type is HTML (e.g. text/html; charset=utf-8)
+	contentType := response.Header.Get("Content-Type")
+	if contentType != "text/html" && !strings.HasPrefix(contentType, "text/html;") {
+		return fmt.Errorf("%s has type %s, not text/html", url, contentType)
+	}
+
+	doc, err := html.Parse(response.Body)
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	visitNode := func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "title" && node.FirstChild != nil {
+			fmt.Println(node.FirstChild.Data)
+		}
+	}
+
+	forEachNode(doc, visitNode, nil)
+	return nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
