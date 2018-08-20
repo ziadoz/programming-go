248e49be94c58c328b166d05d402feaa9cb2359d
Jamie <ziadoz@gmail.com>
2018-08-20 20:27:18 +0100

    Defer closing a resource. OS file closing is an edge case.

---
+++ ch5/ex5.18/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		fmt.Println(fetch(url))
+	}
+}
+
+// Fecth downloads the URL and returns the name and length of the local file.
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+
+	defer func() {
+		// Close file, but prefer error from copy, if any.
+		if closeErr := f.Close(); err == nil {
+			err = closeErr
+		}
+	}()
+
+	n, err = io.Copy(f, resp.Body)
+	return local, n, err
+}
