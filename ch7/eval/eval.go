1e2a4f934330c08ce8784f26e78baf05b876ac10
Jamie <ziadoz@gmail.com>
2018-11-30 21:35:02 +0000

    Eval package part 1.

---
+++ ch7/eval/eval.go
@@ -0,0 +1,81 @@
+package eval
+
+import (
+	"fmt"
+	"math"
+)
+
+// A Var idenitfies a variable, e.g., x.
+type Var string
+
+func (v Var) Eval(env Env) float64 {
+	return env[v]
+}
+
+// A literal is a numeric constant, e.g., 3.141.
+type literal float64
+
+func (l literal) Eval(_ Env) float64 {
+	return float64(l)
+}
+
+// A unary represents a unary operator expression, e.g., -x.
+type unary struct {
+	op rune // one of '+', '-'
+	x  Expr
+}
+
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+// A binary represents a binary operator, e.g., x+y.
+type binary struct {
+	op   rune
+	x, y Expr
+}
+
+func (b binary) Eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.Eval(env) + b.y.Eval(env)
+	case '-':
+		return b.x.Eval(env) - b.y.Eval(env)
+	case '*':
+		return b.x.Eval(env) * b.y.Eval(env)
+	case '/':
+		return b.x.Eval(env) / b.y.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+
+// A call represents a function call expression, e.g., sin(x).
+type call struct {
+	fn   string // one of "pow", "sin", "sqrt"
+	args []Expr
+}
+
+func (c call) Eval(env Env) float64 {
+	switch c.fn {
+	case "pow":
+		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+	case "sin":
+		return math.Sin(c.args[0].Eval(env))
+	case "sqrt":
+		return math.Sqrt(c.args[0].Eval(env))
+	}
+	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
+}
+
+type Env map[Var]float64
+
+type Expr interface {
+	// Eval returns the value of this Expr in the environment env.
+	Eval(env Env) float64
+}
