710b713d4926d20e9cb6f44e30880867625616bd
Jamie <ziadoz@gmail.com>
2019-02-05 21:38:29 +0000

    Eval.

--- ch7/eval/eval.go
+++ ch7/eval/eval.go
@@ -3,6 +3,7 @@ package eval
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 // A Var idenitfies a variable, e.g., x.
@@ -12,11 +13,20 @@ func (v Var) Eval(env Env) float64 {
 	return env[v]
 }
 
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
 // A literal is a numeric constant, e.g., 3.141.
 type literal float64
 
 func (l literal) Eval(_ Env) float64 {
 	return float64(l)
+}
+
+func (literal) Check(vars map[Var]bool) error {
+	return nil
 }
 
 // A unary represents a unary operator expression, e.g., -x.
@@ -33,6 +43,13 @@ func (u unary) Eval(env Env) float64 {
 		return -u.x.Eval(env)
 	}
 	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
 }
 
 // A binary represents a binary operator, e.g., x+y.
@@ -55,6 +72,16 @@ func (b binary) Eval(env Env) float64 {
 	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
 }
 
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("unexpect binary op %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
 // A call represents a function call expression, e.g., sin(x).
 type call struct {
 	fn   string // one of "pow", "sin", "sqrt"
@@ -73,9 +100,29 @@ func (c call) Eval(env Env) float64 {
 	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
 }
 
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d", c.fn, len(c.args), arity)
+	}
+	for _, args := range c.args {
+		if err := args.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
+
 type Env map[Var]float64
 
 type Expr interface {
 	// Eval returns the value of this Expr in the environment env.
 	Eval(env Env) float64
+	// Check reports errors in this Expr and adds its Vars to the set.
+	Check(vars map[Var]bool) error
 }
