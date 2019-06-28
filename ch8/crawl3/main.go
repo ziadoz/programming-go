e5f84c06841a306becc67f32ebaddbe24259cc7a
Jamie <ziadoz@gmail.com>
2019-06-28 18:27:14 +0100

    New crawl version.
    In this 20 goroutines are spun up to process any unseen links. They crawl each link, then spin of a new goroutine that sends these to the worklist.
    
    The main goroutine processes the worklist, sending any unseen links to the channel.

---
+++ ch8/crawl3/main.go
@@ -0,0 +1,52 @@
+// Usage:   findlinks3 [url]
+// Example: findlinks3 http://golang.org
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopl.io/ch5/links"
+)
+
+func main() {
+	worklist := make(chan []string)
+	unseenLinks := make(chan string)
+
+	// Add command-line arguments to worklist.
+	go func() { worklist <- os.Args[1:] }()
+
+	// Create 20 crawler goroutines to fetch each unseen link.
+	for i := 0; i < 20; i++ {
+		go func() {
+			for link := range unseenLinks {
+				foundLinks := crawl(link)
+				go func() { worklist <- foundLinks }()
+			}
+		}()
+	}
+
+	// The main goroutine de-duplicates worklist items
+	// and sends the unseen ones to the crawlers.
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				unseenLinks <- link
+			}
+		}
+	}
+}
+
+func crawl(url string) []string {
+	fmt.Println(url)
+
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return list
+}
