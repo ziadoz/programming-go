08fa647d1c9b17a29169068998e491b5087946b1
Jamie <ziadoz@gmail.com>
2019-08-07 21:38:07 +0100

    Concurrency-safe bank using a binary semaphore.

---
+++ ch9/bank2/bank2.go
@@ -0,0 +1,19 @@
+package bank
+
+var (
+	sema    = make(chan struct{}, 1) // a binary semaphore guarding balance
+	balance int
+)
+
+func Deposit(amount int) {
+	sema <- struct{}{} // acquire token
+	balance = balance + amount
+	<-sema // release token
+}
+
+func Balance() int {
+	sema <- struct{}{} // acquire token
+	b := balance
+	<-sema // release token
+	return b
+}
