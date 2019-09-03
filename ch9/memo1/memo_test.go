caf73c56adb5a8b2280fbee1538579642754ff21
Jamie <ziadoz@gmail.com>
2019-09-03 22:50:15 +0100

    Memoisation with concurrency. v1.

---
+++ ch9/memo1/memo_test.go
@@ -0,0 +1,20 @@
+package memo_test
+
+import (
+	"testing"
+
+	memo "gopl.io/ch9/memo1"
+	"gopl.io/ch9/memotest"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func TestSequential(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Sequential(t, m)
+}
+
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Concurrent(t, m)
+}
