b7e024bb970455b8c3964fd7490db22022181b34
Jamie <ziadoz@gmail.com>
2019-09-05 22:15:45 +0100

    Memoise using channels and goroutines.

---
+++ ch9/memo5/memo.go
@@ -0,0 +1,72 @@
+// Package memo provides a concurrency unsafe
+// memoisation of a function of type Func.
+package memo
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+type entry struct {
+	res   result
+	ready chan struct{} // closed when res is ready
+}
+
+// A request is a message requesting that the Func be applied to the key.
+type request struct {
+	key      string
+	response chan<- result // The client wants a single result
+}
+
+// A Memo caches the result of calling a Func.
+type Memo struct {
+	requests chan request
+}
+
+// Func is the type of the function to memoise.
+type Func func(key string) (interface{}, error)
+
+func New(f Func) *Memo {
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
+}
+
+func (memo *Memo) Get(key string) (interface{}, error) {
+	response := make(chan result)
+	memo.requests <- request{key, response}
+	res := <-response
+	return res.value, res.err
+}
+
+func (memo *Memo) Close() {
+	close(memo.requests)
+}
+
+func (memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+		if e == nil {
+			// This is the first request for this key.
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key) // call f(key)
+		}
+		go e.deliver(req.response)
+	}
+}
+
+func (e *entry) call(f Func, key string) {
+	// Evaluate the function.
+	e.res.value, e.res.err = f(key)
+	// Broadcast the ready condition.
+	close(e.ready)
+}
+
+func (e *entry) deliver(response chan<- result) {
+	// Wait for the ready condition.
+	<-e.ready
+	// Send the result to the client.
+	response <- e.res
+}
