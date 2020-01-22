a27a11633b814e3ced8912aaa1ea270f9fa94a74
Jamie <ziadoz@gmail.com>
2020-01-22 20:59:52 +0000

    Radomised testings with a pseudo random seed.

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -1,6 +1,10 @@
 package word
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestPalindrome(t *testing.T) {
 	var tests = []struct {
@@ -23,6 +27,33 @@ func TestPalindrome(t *testing.T) {
 	for _, test := range tests {
 		if got := IsPalindrome(test.input); got != test.want {
 			t.Errorf(`IsPalindrome(%q) = %v`, test.input, got)
+		}
+	}
+}
+
+// randomPalindrome returns a palindrome whose length and contents
+// are derived from the pseudo random number generator rng.
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25) // Random length up to 24
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000)) // random runes up to '\u0999'
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomisedPalindrome(t *testing.T) {
+	// Initialise a pseudo-random number generator.
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf(`IsPalindrome(%q) = false`, p)
 		}
 	}
 }
